6b22e559a6614190bcec91eaf763573668560c7c
Martin Koidu <martin.koidu@pipedrive.com>
2019-05-11 20:47:33 +0300

    Daily interests logic enhancements

--- src/controllers/interests.ts
+++ src/controllers/interests.ts
@@ -8,13 +8,19 @@ export default async function(ctx: Koa.Context) {
 	const groupBy = ctx.query.groupBy;
 
 	await db
-		.select()
 		.from(type)
 		.modify((queryBuilder: Knex.QueryBuilder) => {
 			if (year && type !== 'daily_interests') {
 				queryBuilder.where('year', year);
 			} else if (year && type == 'daily_interests') {
-				queryBuilder.whereRaw(`YEAR(date) = ${parseInt(year)}`);
+				queryBuilder
+					.select(
+						db.raw(
+							'DAY(date) as day, sum(total) as total, sum(loss) as loss, sum(net) as net'
+						)
+					)
+					.whereRaw(`YEAR(date) = ${parseInt(year)}`)
+					.groupBy('day');
 			}
 
 			if (groupBy) {
