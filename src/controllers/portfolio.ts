20ea087f2c3a1eb3570a57adb568dbdbb306b909
Martin Koidu <martin.koidu@pipedrive.com>
2020-02-02 18:43:08 +0200

    take start and end dates as input for portfolio endpoint

--- src/controllers/portfolio.ts
+++ src/controllers/portfolio.ts
@@ -1,27 +1,33 @@
 import db from '../database';
 import * as Koa from 'koa';
-import * as _ from 'lodash';
+import { uniqBy } from 'lodash';
 
 export default async function(ctx: Koa.Context) {
-	let response;
+    const dateStart = ctx.query.dateStart;
+    const dateEnd = ctx.query.dateEnd;
 
-	if (ctx.query.type === 'by_month') {
-		response = await db
-			.select(db.raw('month(date) as month, source, max(value) as value'))
-			.from('portfolio_values as pvs')
-			.whereRaw(
-				'date IN(SELECT MAX(date) FROM portfolio_values WHERE source = pvs.source group by month(date))'
-			)
-			.groupByRaw('source, month(date)');
-	} else {
-		response = _.uniqBy(
-			await db
-				.select('source as name', 'value')
-				.from('portfolio_values')
-				.orderBy('date', 'desc'),
-			'name'
-		);
-	}
+    let response;
 
-	ctx.response.body = JSON.stringify(response);
+    if (ctx.query.type === 'by_month') {
+        response = await db
+            .select(db.raw('month(date) as month, source, max(value) as value'))
+            .from('portfolio_values as pvs')
+            // .whereBetween('date', [periodStart, periodEnd])
+            .whereRaw(
+                `date IN(SELECT MAX(date) FROM portfolio_values WHERE source = pvs.source AND date BETWEEN '${dateStart}' AND '${dateEnd}' group by month(date))`
+                // `date IN(SELECT MAX(date) FROM portfolio_values WHERE source = pvs.source group by month(date))`
+            )
+            .groupByRaw('source, month(date)');
+    } else {
+        response = uniqBy(
+            await db
+                .select('source as name', 'value')
+                .from('portfolio_values')
+                .whereBetween('date', [dateStart, dateEnd])
+                .orderBy('date', 'desc'),
+            'name'
+        );
+    }
+
+    ctx.response.body = JSON.stringify(response);
 }
